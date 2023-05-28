11661cc8ca4f03c82cae474ee885e233ab6d0287
soulivore <iwonttellyou@myemail.address>
2023-05-28 14:52:24 -0600

    moved over servers from old laptop

---
+++ servers/update_CrystalIsles_2.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+SERVER_PATH=/home/$USER/Ark/servers/server_CrystalIsles_2/
+
+# mods
+SUPER_SPYGLASS=793605978
+
+echo "updating server..."
+steamcmd +force_install_dir "$SERVER_PATH" +login anonymous +app_update 376030 +quit
+
+echo "updating mods..."
+python3 /home/$USER/Ark/scripts/Ark_Mod_Downloader.py --modids "$SUPER_SPYGLASS" --workingdir "$SERVER_PATH" --steamcmd "/usr/games" --namefile
